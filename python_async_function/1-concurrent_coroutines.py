450f401beacc296ad43601054e87332ef1b9b705
JulienGirardey <juliengirardey2@gmail.com>
2025-07-29 11:13:11 +0200

    Refactor import order in 1-concurrent_coroutines.py for improved clarity

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -3,9 +3,9 @@
 you’ve written and write an async routine called
 wait_n that takes in 2 int arguments
 """
-wait_random = __import__('0-basic_async_syntax').wait_random
-from typing import List
 import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
