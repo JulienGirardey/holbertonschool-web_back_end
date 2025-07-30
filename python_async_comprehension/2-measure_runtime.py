24369b16a116d958c937047a0ec7b4d61876bd02
JulienGirardey <juliengirardey2@gmail.com>
2025-07-30 15:14:31 +0200

    Fix formatting in measure_runtime docstring and ensure async_generator import is consistent

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -12,8 +12,8 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 async def measure_runtime() -> float:
     """
     measure_runtime should measure the total runtime and return it.
-	Notice that the total runtime is roughly 10 seconds,
- 	explain it to yourself
+        Notice that the total runtime is roughly 10 seconds,
+        explain it to yourself
     """
     start_time = time.time()
     await asyncio.gather(*(async_comprehension() for _ in range(4)))
