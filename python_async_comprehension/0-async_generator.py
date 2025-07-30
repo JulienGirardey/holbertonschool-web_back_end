85cba7080d036a474bcb0907c43fa5bf8d6822fc
JulienGirardey <juliengirardey2@gmail.com>
2025-07-30 14:57:32 +0200

    Enhance docstring for async_generator function to clarify its behavior and purpose

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -7,6 +7,11 @@ import random
 
 
 async def async_generator():
+    """
+    The coroutine will loop 10 times, each time asynchronously
+    wait 1 second, then yield a random number between 0 and 10.
+    Use the random module.
+    """
     for _ in range(10):
         await asyncio.sleep(1)
         result = random.uniform(0, 10)
