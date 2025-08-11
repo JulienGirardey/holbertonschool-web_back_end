ec6cbbd987affd9ef4ad86f65ebfa15afddf716d
JulienGirardey <juliengirardey2@gmail.com>
2025-08-11 10:41:17 +0200

    Add an empty README file to the pagination directory

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+this file contains a function index_range
+that takes two integer
+arguments page and page_size
+"""
+
+
+def index_range(page, page_size):
+    return ((page - 1) * page_size, (page - 1) * page_size + page_size)
