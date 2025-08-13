b67ec23b0ebeeb3287f8e43db1a93c5c5473c2ca
JulienGirardey <juliengirardey2@gmail.com>
2025-08-13 10:21:34 +0200

    Implement list_all function for MongoDB collection retrieval
    - Removed the _re.py file from the tomli package as it is no longer needed.
    - Added a new file 8-all.py containing the list_all function, which retrieves all documents from a specified MongoDB collection.
    - Created a main script 8-main.py to demonstrate the usage of the list_all function, connecting to a MongoDB instance and printing the retrieved documents.

---
+++ NoSQL/8-all.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+this file contains the function list_all(mongo_collection)
+"""
+
+
+def list_all(mongo_collection):
+    """
+    that lists all document in the collection
+    """
+    if mongo_collection is None:
+        return []
+    else:
+        return mongo_collection.find()
