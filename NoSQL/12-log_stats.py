c8bdac1f120cf7a7b2a6ae4d8f935b325fd8601f
JulienGirardey <juliengirardey2@gmail.com>
2025-08-14 09:43:17 +0200

    Refactor log_stats.py to improve code clarity and consistency

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -7,20 +7,17 @@ from pymongo import MongoClient
 
 
 if __name__ == "__main__":
-    """
-    that provides some stats about Nginx logs stored in MongoDB
-    """
     client = MongoClient('mongodb://127.0.0.1:27017')
     stats = client.logs.nginx
 
     nb_documents = stats.estimated_document_count()
-    method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
-    print(nb_documents, " logs")
+    print("{} logs".format(nb_documents))
     print("Methods:")
     count = 0
-    for i in method:
-        school = stats.count_documents({'method': i})
-        count += school
-        print("    method {}: {}".format(i, school))
-    print("{} status check".format(stats.count_documents({'path': '/status'})))
+    for method in methods:
+        count = stats.count_documents({'method': method})
+        print("    method {}: {}".format(method, count))
+    status_check = stats.count_documents({'method': 'GET', 'path': '/status'})
+    print("{} status check".format(status_check))
