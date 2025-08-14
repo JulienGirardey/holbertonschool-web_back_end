2644e134ab8414749a286b4c6b5e07d407e73bf3
JulienGirardey <juliengirardey2@gmail.com>
2025-08-14 09:48:12 +0200

    Add tomli package and update log_stats.py
    - Added tomli package version 2.2.1 with necessary files including __init__.py, _parser.py, _re.py, and _types.py.
    - Implemented parsing functionality for TOML files.
    - Updated 12-log_stats.py to correct a typo in the docstring and optimized the method count logic.

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-this file is a programe, that provides some
+this file is a program, that provides some
 stats about Nginx logs stored in MongoDB
 """
 from pymongo import MongoClient
@@ -15,9 +15,9 @@ if __name__ == "__main__":
 
     print("{} logs".format(nb_documents))
     print("Methods:")
-    count = 0
     for method in methods:
         count = stats.count_documents({'method': method})
         print("    method {}: {}".format(method, count))
+
     status_check = stats.count_documents({'method': 'GET', 'path': '/status'})
     print("{} status check".format(status_check))
