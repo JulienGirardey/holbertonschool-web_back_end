6c88c734cafca510673842a0fab9920b6408ccb8
JulienGirardey <juliengirardey2@gmail.com>
2025-08-14 08:57:31 +0200

    Refactor log_stats.py to use consistent variable names for MongoDB collections

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -8,16 +8,17 @@ from pymongo import MongoClient
 
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
-    stats = client.logs.nginx
+    mydb = client["logs"]
+    mycol = mydb["nginx"]
 
-    nb_documents = stats.estimated_document_count()
+    nb_documents = mycol.estimated_document_count()
     method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
     print(nb_documents, " logs")
     print("Methods:")
     count = 0
     for i in method:
-        school = stats.count_documents({'method': i})
+        school = mycol.count_documents({'method': i})
         count += school
         print("    method {}: {}".format(i, school))
-    print("{} status check".format(stats.count_documents({'path': '/status'})))
+    print("{} status check".format(mycol.count_documents({'path': '/status'})))
