bae1f0d003dfdc2d0277a721251f42510f63cb39
JulienGirardey <juliengirardey2@gmail.com>
2025-08-19 10:02:40 +0200

    Add block scope demonstration in taskBlock function

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -2,5 +2,9 @@ export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
+  if (trueOrFalse) {
+	// Intentionally left blank for block scope demonstation
+  }
+
   return [task, task2];
 }
