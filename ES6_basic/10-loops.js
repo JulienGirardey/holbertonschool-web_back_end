e0d890b17839485b93406e23ed3d012f217269db
JulienGirardey <juliengirardey2@gmail.com>
2025-08-19 14:08:58 +0200

    Fix formatting by adding a newline at the end of the file

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   let idx = 0;
   for (const value of array) {
-    array[idx] = appendString + value;
+    array[idx] = appendString + value;  
     idx += 1;
   }
   return array;
