8873df57b244cd6f6a0b851010102b621b20be84
JulienGirardey <juliengirardey2@gmail.com>
2025-08-20 13:13:20 +0200

    Add type validation for maxStudentsSize in ClassRoom constructor

--- ES6_classes/0-classroom.js
+++ ES6_classes/0-classroom.js
@@ -1,8 +1,9 @@
 export default class ClassRoom {
 	constructor(maxStudentsSize) {
-		if (typeof maxStudentsSize !== "number") {
-			throw new TypeError("maxStudentsSize must be a number");
-		}
 		this._maxStudentsSize = maxStudentsSize;
+	}
+
+	get maxStudentsSize() {
+		return this._maxStudentsSize;
 	}
 }
