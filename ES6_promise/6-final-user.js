b53387f86e39363c3b4e6571c96189f26e1c2c7b
JulienGirardey <juliengirardey2@gmail.com>
2025-08-27 11:20:04 +0200

    fix: correct promise handling in handleProfileSignup function

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,5 +2,20 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-	return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
+	return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+		.then((results) => {
+			return results.map(result => {
+				if (value === "fulfilled") {
+					return {
+						status: result.status,
+						value: result.value,
+					};
+				} else {
+					return {
+						status: result.status,
+						value: result.reason,
+					};
+				}
+			});
+		})
 }
