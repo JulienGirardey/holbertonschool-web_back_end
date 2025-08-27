bf628495a66686e439154723d8c61fea5b24b442
JulienGirardey <juliengirardey2@gmail.com>
2025-08-27 11:25:07 +0200

    fix: simplify result mapping in handleProfileSignup function

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -4,18 +4,9 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
 	return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
 		.then((results) => {
-			return results.map(result => {
-				if (result.value === "fulfilled") {
-					return {
-						status: result.status,
-						value: result.value,
-					};
-				} else {
-					return {
-						status: result.status,
-						value: result.reason,
-					};
-				}
-			});
-		})
+			results.map(result => ({
+				status: result.status,
+				value: result.value === "fulfilled" ? result.value : result.reason,
+			}))
+		});
 }
