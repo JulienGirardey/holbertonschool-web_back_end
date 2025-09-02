b59a7ad1e270a062c28d4bffc0d3b3699645e94d
JulienGirardey <juliengirardey2@gmail.com>
2025-09-02 09:37:53 +0200

    fix: remove unnecessary blank lines in console and stdin files

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -5,16 +5,13 @@
  * display 'This important software is now closing'
  **/
 process.stdin.setDefaultEncoding('utf-8');
-
 console.log("Welcome to Holberton School, what is your name?");
-
 process.stdin.on('readable', function() {
 	var chunk = process.stdin.read();
 	if (chunk !== null) {
 		process.stdout.write('Your name is: ' + chunk);
 	}
 });
-
 process.stdin.on('end', function() {
 	process.stdout.write('This important software is now closing\n');
 });
