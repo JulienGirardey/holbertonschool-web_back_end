2f414c85ea311a298087ceff3c24482ed3968674
JulienGirardey <juliengirardey2@gmail.com>
2025-09-02 15:45:29 +0200

    fix: remove unnecessary blank lines in countStudents function

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -6,12 +6,12 @@ async function countStudents(path) {
       const db = await readFile(path, 'utf8');
       const lines = db.split('\n').filter(line => line.trim() !== '');
       const students = lines.slice(1);
-    
+
       let result = `Number of students: ${students.length}\n`;
 
       const cs = students.filter(line => line.split(',')[3] === 'CS');
       result += `Number of students in CS: ${cs.length}. List: ${cs.map(line => line.split(',')[0]).join(', ')}\n`;
-    
+
       const swe = students.filter(line => line.split(',')[3] === 'SWE');
       result += `Number of students in SWE: ${swe.length}. List: ${swe.map(line => name = line.split(',')[0]).join(', ')}`;
 
