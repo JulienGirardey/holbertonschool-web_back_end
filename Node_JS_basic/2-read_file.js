6bed103436d287357f4a2af0b8debf0d53149ba3
JulienGirardey <juliengirardey2@gmail.com>
2025-09-02 15:52:03 +0200

    fix: standardize indentation in 2-read_file.js for consistency

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,21 +1,21 @@
 const { readFileSync } = require('fs');
 
 function countStudents(path) {
-	try {
-		const db = readFileSync(path).toString();
-		const lines = db.split('\n').filter(line => line.trim() !== '');
-		const students = lines.slice(1);
+  try {
+    const db = readFileSync(path).toString();
+    const lines = db.split('\n').filter(line => line.trim() !== '');
+    const students = lines.slice(1);
 
-		console.log(`Number of students: ${students.length}`);
-		const cs = students.filter(line => line.split(',')[3] === 'CS');
-		console.log(`Number of students in CS: ${cs.length}. List: ${cs.map(line => line.split(',')[0]).join(', ')}`);
+    console.log(`Number of students: ${students.length}`);
+    const cs = students.filter(line => line.split(',')[3] === 'CS');
+    console.log(`Number of students in CS: ${cs.length}. List: ${cs.map(line => line.split(',')[0]).join(', ')}`);
 
-		const swe = students.filter(line => line.split(',')[3] === 'SWE');
-		console.log(`Number of students in SWE: ${swe.length}. List: ${swe.map(line => name = line.split(',')[0]).join(', ')}`);
+    const swe = students.filter(line => line.split(',')[3] === 'SWE');
+    console.log(`Number of students in SWE: ${swe.length}. List: ${swe.map(line => name = line.split(',')[0]).join(', ')}`);
 
-	} catch {
-		throw new Error("Cannot load the database");
-	}
+  } catch {
+    throw new Error("Cannot load the database");
+  }
 }
 
 module.exports = countStudents;
