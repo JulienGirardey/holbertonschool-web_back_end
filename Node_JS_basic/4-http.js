1e16860755622d613c4dfc27dcd995c123be6b48
JulienGirardey <juliengirardey2@gmail.com>
2025-09-03 13:33:31 +0200

    fix: simplify http module import and remove unnecessary logging

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,17 +1,12 @@
-const http = require('node:http');
+const http = require('http');
 
 const app = http.createServer((request, response) => {
-	const { method, url } = request;
-
-	console.log(`Received ${method} request for: ${url}`);
 
 	response.writeHead(200, { 'content-type': 'text/plain' });
 
 	response.end('Hello Holberton School!\n');
 });
 
-app.listen(1245, () => {
-	console.log('Server is running at http://localhost:1245/');
-});
+app.listen(1245);
 
 module.exports = app;
