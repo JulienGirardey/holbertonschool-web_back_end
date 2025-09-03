6dee84b32ecd6f174cc7f43155ee8cb6d4c462c5
JulienGirardey <juliengirardey2@gmail.com>
2025-09-03 14:00:07 +0200

    fix: remove unused console logging and clean up code structure

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,33 +1,28 @@
 const http = require('node:http');
 const countStudents = require('./3-read_file_async');
-const { count } = require('node:console');
 
 const app = http.createServer(async (request, response) => {
-	const { method, url } = request;
+  const { url } = request;
 
-	console.log(`Received ${method} request for: ${url}`);
+  response.writeHead(200, { 'content-Type': 'text/plain' });
 
-	response.writeHead(200, { 'content-Type': 'text/plain' });
+  if (url === '/') {
+    response.end('Hello Holberton School!\n');
+  } else if (url === '/students') {
+    const dbName = process.argv[2] || '';
 
-	if (url === '/') {
-		response.end('Hello Holberton School!\n');
-	} else if (url === '/students') {
-		const dbName = process.argv[2] || '';
-
-		countStudents(dbName)
-			.then((data) => {
-				response.end(`This is the list of our students\n${data}`);
-			})
-			.catch(() => {
-				response.end('Cannot load the database\n');
-			});
-	} else {
-		response.end('Not found\n');
-	}
+    countStudents(dbName)
+      .then((data) => {
+        response.end(`This is the list of our students\n${data}`);
+      })
+      .catch(() => {
+        response.end('Cannot load the database\n');
+      });
+  } else {
+    response.end('Not found\n');
+  }
 });
 
-app.listen(1245, () => {
-	console.log('Server is running at http://localhost:1245/');
-});
+app.listen(1245);
 
 module.exports = app;
