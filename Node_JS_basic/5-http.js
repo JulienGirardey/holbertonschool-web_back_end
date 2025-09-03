5be75a05541676fa33c915a786972b2a65d5255c
JulienGirardey <juliengirardey2@gmail.com>
2025-09-03 14:06:16 +0200

    fix: standardize content-type header casing and refactor student retrieval logic

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,23 +1,28 @@
 const http = require('node:http');
 const countStudents = require('./3-read_file_async');
+const { count } = require('node:console');
 
 const app = http.createServer(async (request, response) => {
   const { url } = request;
 
-  response.writeHead(200, { 'content-Type': 'text/plain' });
+  response.writeHead(200, { 'Content-Type': 'text/plain' });
 
   if (url === '/') {
     response.end('Hello Holberton School!\n');
   } else if (url === '/students') {
     const dbName = process.argv[2] || '';
+    let logs = '';
+    const originalLog = console.log;
+    console.log = (msg) => { logs += `${msg}\n`;};
 
-    countStudents(dbName)
-      .then((data) => {
-        response.end(`This is the list of our students\n${data}`);
-      })
-      .catch(() => {
-        response.end('Cannot load the database\n');
-      });
+    try {
+      await countStudents(dbName);
+      console.log = originalLog;
+      response.end(`This is the list of our students\n${logs.trim()}`);
+    } catch {
+      console.log = originalLog;
+      response.end(`This is the list of our students\nCannot load the database`);
+    }
   } else {
     response.end('Not found\n');
   }
