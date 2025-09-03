5c80a07abb6408285060f7f996b322c6f1b22c35
JulienGirardey <juliengirardey2@gmail.com>
2025-09-03 14:26:44 +0200

    fix: format code for consistent indentation in root endpoint handler

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -2,9 +2,8 @@ const express = require('express');
 const app = express();
 const port = 1245;
 
-
 app.get('/', (req, res) => {
-	res.end('Hello Holberton School!');
+  res.end('Hello Holberton School!');
 });
 
 app.listen(port);
