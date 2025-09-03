d68f9d0204df8f49e165adfedb33155e3c1af7b2
JulienGirardey <juliengirardey2@gmail.com>
2025-09-03 13:34:55 +0200

    fix: remove unnecessary newlines in response end message

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,10 +1,8 @@
 const http = require('http');
 
 const app = http.createServer((request, response) => {
-
 	response.writeHead(200, { 'content-type': 'text/plain' });
-
-	response.end('Hello Holberton School!\n');
+	response.end('Hello Holberton School!');
 });
 
 app.listen(1245);
