a7cab23d5411a38e13c22049726ffcbc0637fd39
JulienGirardey <juliengirardey2@gmail.com>
2025-09-03 15:06:29 +0200

    fix: improve variable naming and logging capture in /students endpoint for clarity

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,36 +1,42 @@
-const countStudents = require('./3-read_file_async');
-
 const express = require('express');
+const countStudents = require('./3-read_file_async');
 
 const app = express();
 
+// Route pour la racine
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
 app.get('/students', (req, res) => {
-  const dbName = process.argv[2] || '';
-  let Text = 'This is the list of our students\n';
+  const databasePath = process.argv[2];
+  let responseText = 'This is the list of our students\n';
 
-  let logs = '';
+  // Sauvegarde console.log
   const originalLog = console.log;
+  let capturedOutput = '';
 
-  console.log = (msg) => { logs += `${msg}\n`; };
+  // Capture console.log
+  console.log = (message) => {
+    capturedOutput += `${message}\n`;
+  };
 
-  countStudents(dbName)
+  // Appeler countStudents
+  countStudents(databasePath)
     .then(() => {
+      // Restaurer console.log
       console.log = originalLog;
 
-      Text += logs;
-      res.send(Text);
+      // Envoie la réponse complete
+      responseText += capturedOutput;
+      res.send(responseText);
     })
     .catch(() => {
       console.log = originalLog;
-
-      Text += 'Cannot load the database';
-      res.end(Text);
+      responseText += 'Cannot load the database';
+      res.send(responseText);
     });
-})
+});
 
 app.listen(1245);
 
