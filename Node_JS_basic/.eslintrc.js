de7f42544d4a54f4a921daa2cdf754bc79d760fa
JulienGirardey <juliengirardey2@gmail.com>
2026-01-08 15:55:16 +0100

    fix: update dependencies and scripts in package.json
    - Changed test script to use local mocha binary.
    - Updated dev script to point to the correct server file.
    - Downgraded express version to 4.17.1 for compatibility.
    - Updated eslint and related packages to maintain code quality.
    - Adjusted mocha version to 6.2.3 for stability.

--- Node_JS_basic/.eslintrc.js
+++ Node_JS_basic/.eslintrc.js
@@ -2,10 +2,11 @@ module.exports = {
   env: {
     browser: false,
     es6: true,
-    node: true,
+    jest: true,
   },
   extends: [
     'airbnb-base',
+    'plugin:jest/all',
   ],
   globals: {
     Atomics: 'readonly',
@@ -15,8 +16,7 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: [
-  ],
+  plugins: ['jest'],
   rules: {
     'max-classes-per-file': 'off',
     'no-underscore-dangle': 'off',
